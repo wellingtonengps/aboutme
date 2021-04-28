07da25c8a2db7b85965f8f6d03ca925e2da5bc74
Wellington <wellington.engps@gmail.com>
2021-04-28 17:19:32 -0300

    sobre apra index

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,27 +5,27 @@ import HeaderWrapperStyles from './syles.module'
 export default function Header() {
     return (
         <HeaderWrapperStyles>
-            
+
             <ul>
                 <li>
-                <Link href="http://localhost:3000/sobre">
-                    <a>Sobre</a>
-                </Link>
+                    <Link href="http://localhost:3000/">
+                        <a>Sobre</a>
+                    </Link>
                 </li>
                 <li>
-                <Link href="http://localhost:3000/competencias">
-                    <a>Competências</a>
-                </Link>
+                    <Link href="http://localhost:3000/competencias">
+                        <a>Competências</a>
+                    </Link>
                 </li>
                 <li>
-                <Link href="http://localhost:3000/formacao">
-                    <a>Formação</a>
-                </Link>
+                    <Link href="http://localhost:3000/formacao">
+                        <a>Formação</a>
+                    </Link>
                 </li>
                 <li>
-                <Link href="http://localhost:3000/projetos">
-                    <a>Projetos</a>
-                </Link>
+                    <Link href="http://localhost:3000/projetos">
+                        <a>Projetos</a>
+                    </Link>
                 </li>
             </ul>
         </HeaderWrapperStyles>
