fee2696a75574d58f6c7406f10c8b25e6664e4b3
wellington <wellington.engps@gmail.com>
2024-05-08 20:57:36 -0300

    card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
@@ -6,14 +7,20 @@ export default function Home() {
         <div className="flex flex-col md:flex-row items-center">
             <Image src="/profile.svg" alt="profile" height={245} width={245}/>
             <div className="flex flex-col items-center md:items-start md:ml-20  mt-8 md:mt-0 ">
-                <h1 className="text-2xl md:text-start">Wellingto Pereira Silva</h1>
+                <h1 className="text-2xl md:text-start">Wellington Pereira Silva</h1>
                 <p className="mt-3 text-justify md:text-start">Olá, sou Wellington estudante de Ciência da Computação, estagiário em suporte de TI
                     no Hospital Unimed Juiz de Fora, com experiência em desenvolvimento Web e mobile,
                     com aptidão também em UX/UI, buscando uma oportunidade como desenvolver.</p>
             </div>
         </div>
-        <div className="">
-
+        <div className="mt-8">
+            <h1 className="mb-4 text-center text-2xl">Hard-Skills</h1>
+            <div className="grid grid-cols-1 grid-flow-row gap-5 md:grid-rows-1 md:grid-flow-col">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
         </div>
     </main>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+
+export default function Card() {
+    return (
+        <div className="flex flex-col justify-between h-32 bg-blue-600 p-4 text-center rounded-md">
+            <h1>Liguagem de programação</h1>
+            <div className="flex flex-row text-center justify-center">
+                <div className="flex justify-center items-center h-10 w-10 bg-green-500">oi</div>
+                <div className="flex justify-center items-center h-10 w-10 bg-green-500">oi</div>
+                <div className="flex justify-center items-center h-10 w-10 bg-green-500">oi</div>
+                <div className="flex justify-center items-center h-10 w-10 bg-green-500">oi</div>
+            </div>
+        </div>
+    )
+}
