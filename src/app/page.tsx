6a5d7ae4063ff5b324790181e7486d3a0c594fba
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-05-08 12:48:07 -0300

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-between p-24">
-        <div className="flex flex-row">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-col md:flex-row items-center">
             <Image src="/profile.svg" alt="profile" height={245} width={245}/>
-            <div className="flex flex-col items-start">
-                <h1>Wellingto Pereira Silva</h1>
-                <p>Olá, sou Wellington estudante de Ciência da Computação, estagiário em suporte de TI
+            <div className="flex flex-col items-center md:items-start md:ml-20  mt-8 md:mt-0 ">
+                <h1 className="text-2xl md:text-start">Wellingto Pereira Silva</h1>
+                <p className="mt-3 text-justify md:text-start">Olá, sou Wellington estudante de Ciência da Computação, estagiário em suporte de TI
                     no Hospital Unimed Juiz de Fora, com experiência em desenvolvimento Web e mobile,
                     com aptidão também em UX/UI, buscando uma oportunidade como desenvolver.</p>
             </div>
+        </div>
+        <div className="">
+
         </div>
     </main>
   );
